ce8e28b76f28033d8428f4746cd4d29ef8b4b61d
Vaishali Kambli <Vaishali.Kambli1@outlook.com>
2022-07-22 23:06:01 -0400

    Removed commented code

--- carSearching/carSearching.js
+++ carSearching/carSearching.js
@@ -3,28 +3,17 @@ import getAllCarRecords from '@salesforce/apex/CarDetails.getAllCarRecords';
 
 export default class CarSearching extends LightningElement {
   @api carData = [];
-  //label;
-  //fieldName;
-  //type;
-  // @api tableColumns = [
-  //                 { label: 'Name', fieldName: 'Name', type: 'String' },
-  //                 { label: 'Car Model', fieldName: 'Car_Model__c', type: 'String' },
-  //                 { label: 'Car Type', fieldName: 'Car_Type__c', type: 'String' },
-  //                 { label: 'Price - (Per day)', fieldName: 'Price__c', type: 'String' }                                    
-  //               ];
 
-  // connectedCallback(event){
-  //   this.searchRelatedAccounts(event);
-  //   console.log('Inside connected callback');
-  // }
-
+  /**
+   * 
+   * Functionto to Search car for rent
+   */
   searchCarDetails(event){
     let like = event.target.value;
     
     getAllCarRecords({searchKeyword : like}).then(result => {
       if(result){
         this.carData = result;
-        console.log('Response :: ' + this.carData);
       }
     }).catch(error => {
       console.log('Error :: ' + JSON.stringify(error));
