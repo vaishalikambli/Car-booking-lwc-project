c4f803e9b3edb508bfe77566b820a0b7a93fa454
Vaishali Kambli <Vaishali.Kambli1@outlook.com>
2022-07-23 17:17:01 -0400

    Update newCarDetails.js

--- newCarDetails/newCarDetails.js
+++ newCarDetails/newCarDetails.js
@@ -1,5 +1,5 @@
 import { LightningElement, wire } from 'lwc';
-import { fireEvent } from 'c/pubsub';
+//import { fireEvent } from 'c/pubsub';
 import { CurrentPageReference } from 'lightning/navigation';
 import { createRecord } from 'lightning/uiRecordApi';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
@@ -33,68 +33,57 @@ export default class NewCarDetails extends LightningElement {
     }
   }
 
+validateCarFields(){
+  let carNameField = this.template.querySelector('.carName');
+  let carNameValue = carNameField.value;
+  let carModelField = this.template.querySelector('.carModel');
+  let carModelValue = carModelField.value;
+  let carTypeField = this.template.querySelector('.carType');
+  let carTypeValue = carTypeField.value;
+  let pricePerDayField = this.template.querySelector('.pricePerDay');
+  let pricePerDayValue = pricePerDayField.value;
 
-//   validateFields() {
-//     console.log('Inside validate methode');
-//     this.template.querySelectorAll('lightning-input').forEach(element => {
-//         element.reportValidity();
-//     });
-// }
+  if(carNameValue === ''){
+    console.log('Inside validation!!! ' + carNameValue);
+    carNameField.setCustomValidity("Car name value is required");
+  }else{
+    carNameField.setCustomValidity("");
+  }
+  carNameField.reportValidity();
+
+  if(carModelValue === ''){
+    console.log('Inside validation!!! ' + carModelValue);
+    carModelField.setCustomValidity("Car model value is required");
+  }else{
+    carModelField.setCustomValidity("");
+  }
+  carModelField.reportValidity();
+  
+  if(carTypeValue === ''){
+    console.log('Inside validation!!! ' + carTypeValue);
+    carTypeField.setCustomValidity("Car type value is required");
+  }else{
+    carTypeField.setCustomValidity("");
+  }
+  carTypeField.reportValidity();
+  
+  if(pricePerDayValue === ''){
+    console.log('Inside validation!!! ' + pricePerDayValue);
+    pricePerDayField.setCustomValidity("price value is required");
+  }else{
+    pricePerDayField.setCustomValidity("");
+  }
+  pricePerDayField.reportValidity();  
+}
 
   createNewCarEntry(event){
     const fields = {'Name' : this.carName, 'Car_Model__c' : this.carModel, 'Car_Type__c' : this.carType, 'Price__c' : this.pricePerDay};
     const recordDetails = {apiName : 'Car__c', fields : fields};
 
-    let carNameField = this.template.querySelector('.carName');
-    let carNameValue = carNameField.value;
-    let carModelField = this.template.querySelector('.carModel');
-    let carModelValue = carModelField.value;
-    let carTypeField = this.template.querySelector('.carType');
-    let carTypeValue = carTypeField.value;
-    let pricePerDayField = this.template.querySelector('.pricePerDay');
-    let pricePerDayValue = pricePerDayField.value;            
-    console.log('carNameField :: ' + carNameField + ' Car name value :: ' + carNameValue);
-
-
-    //this.saveRecordToast('Error!', 'Please enter car name', 'error');
-    //return;
-
-    if(carNameValue === ''){
-      console.log('Inside validation!!! ' + carNameValue);
-      carNameField.setCustomValidity("Car name value is required");
-    }else{
-      carNameField.setCustomValidity("");
-    }
-
-    if(carModelValue === ''){
-      console.log('Inside validation!!! ' + carModelValue);
-      carModelField.setCustomValidity("Car model value is required");
-    }else{
-      carModelField.setCustomValidity("");
-    }
+    this.validateCarFields();
     
-    if(carTypeValue === ''){
-      console.log('Inside validation!!! ' + carTypeValue);
-      carTypeField.setCustomValidity("Car type value is required");
-    }else{
-      carTypeField.setCustomValidity("");
-    }
-    
-    if(pricePerDayValue === ''){
-      console.log('Inside validation!!! ' + pricePerDayValue);
-      pricePerDayField.setCustomValidity("price value is required");
-    }else{
-      pricePerDayField.setCustomValidity("");
-    }    
-
-    console.log('executed validate method');
     createRecord(recordDetails).then(response => {
-        this.saveRecordToast('Success!', 'You have been created new record Successfully', 'success');
-        // fields.carName = '';
-        // this.carModel = '';
-        // this.carType = 'None';
-        // this.pricePerDay = '';
-        
+        this.saveRecordToast('Success!', 'You have been created new record Successfully', 'success');        
       }).catch(error => {
         this.saveRecordToast('Error!', 'Failed to create new record', 'error');
       })
